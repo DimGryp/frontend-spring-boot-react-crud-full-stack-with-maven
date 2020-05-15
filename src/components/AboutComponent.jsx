a1d7202dd7131019356d1b9d9cee4ca851de12d4
DimGryp <gripeosd@yahoo.com>
2020-05-15 19:53:27 +0300

    Added About Component

---
+++ src/components/AboutComponent.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+
+class AboutComponent  extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>This is an About page</h2>
+            </div>
+        );
+    }
+}
+export default AboutComponent
