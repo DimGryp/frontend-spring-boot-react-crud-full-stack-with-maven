f9ed3e9659799eb9fa6719fce63ae61c57a80075
DimGryp <46029873+DimGryp@users.noreply.github.com>
2020-05-19 12:38:08 +0300

    added test100 remote

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import InstructorApp from './components/InstructorApp';
 
+//added test100 remote
 
 class App extends Component {
   render() {
